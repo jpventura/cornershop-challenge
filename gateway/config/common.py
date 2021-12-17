jpventura/weather-app

feat(gateway): create api gateway django project
The project is structured as follows:

    .
    ├── .env                     # Environment variables
    ├── cornershop               # Django reusable apps
    │   └── apps
    │       └── ...
    ├── gateway                  # Django project directory
    │   ├── config
    │   │   ├── common.py        # All environment common configuration
    │   │   ├── local.py         # Local only environment configuration
    │   │   └── production.py    # Production only environment configuration
    │   ├── urls.py              # API Gateway routes
    │   └── wsgi.py
    └── manage.py

References:

  - cookiecutter: command-line utility for templating python projects
    https://github.com/cookiecutter/cookiecutter

  - cookiecutter-django-rest: DRF API builder with Python3
    http://agconti.github.io/cookiecutter-django-rest/

gateway/config/common.py
@@ -1,0 +1,199 @@
+import os
+
+from distutils.util import strtobool
+from os import path
+
+from configurations import Configuration
+
+
+class Common(Configuration):
+
+    BASE_DIR = path.dirname(path.abspath(path.pardir))
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+
+
+        # Third party apps
+        'django_filters',            # for filtering rest endpoints,
+        'rest_framework',            # utilities for rest apis
+        'rest_framework.authtoken',  # token authentication
+    )
+
+    # https://docs.djangoproject.com/en/2.0/topics/http/middleware/
+    MIDDLEWARE = (
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ALLOWED_HOSTS = ["*"]
+    ROOT_URLCONF = 'gateway.urls'
+    SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+    WSGI_APPLICATION = 'gateway.wsgi.application'
+
+    # Email
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    ADMINS = (
+        ('Author', 'engineer@jpventura.com'),
+    )
+
+    # PostgreSQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': path.join(BASE_DIR, 'cornershop.db'),
+        }
+    }
+
+    # General
+    APPEND_SLASH = False
+    TIME_ZONE = 'UTC'
+    LANGUAGE_CODE = 'en-us'
+    # If you set this to False, Django will make some optimizations so as not
+    # to load the internationalization machinery.
+    USE_I18N = False
+    USE_L10N = True
+    USE_TZ = True
+    LOGIN_REDIRECT_URL = '/'
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/2.0/howto/static-files/
+    STATIC_ROOT = path.normpath(path.join(path.dirname(BASE_DIR), 'static'))
+    STATICFILES_DIRS = []
+    STATIC_URL = '/static/'
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+
+    # Media files
+    MEDIA_ROOT = path.join(path.dirname(BASE_DIR), 'media')
+    MEDIA_URL = '/media/'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': STATICFILES_DIRS,
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+    # Set DEBUG to False as a default for safety
+    # https://docs.djangoproject.com/en/dev/ref/settings/#debug
+    DEBUG = strtobool(os.getenv('DJANGO_DEBUG', 'no'))
+
+    # Password Validation
+    # https://docs.djangoproject.com/en/2.0/topics/auth/passwords/#module-django.contrib.auth.password_validation
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+    # Cornershop API User
+    AUTH_USER_MODEL = 'cornershop.apps.users.User'
+
+    # Logging
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'django.server': {
+                '()': 'django.utils.log.ServerFormatter',
+                'format': '[%(server_time)s] %(message)s',
+            },
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
+        },
+        'filters': {
+            'require_debug_true': {
+                '()': 'django.utils.log.RequireDebugTrue',
+            },
+        },
+        'handlers': {
+            'django.server': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'django.server',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler'
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'propagate': True,
+            },
+            'django.server': {
+                'handlers': ['django.server'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'django.request': {
+                'handlers': ['mail_admins', 'console'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'INFO'
+            },
+        }
+    }
+
+    # Django REST Framework
+    REST_FRAMEWORK = {
+        'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.TokenAuthentication',
+        ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ],
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer',
+        ),
+        'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
+    }

jpventura/cornershop-challenge

gateway/config/common.py
@@ -1,0 +1,199 @@
+import os
+
+from distutils.util import strtobool
+from os import path
+
+from configurations import Configuration
+
+
+class Common(Configuration):
+
+    BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+
+
+        # Third party apps
+        'django_filters',            # for filtering rest endpoints,
+        'rest_framework',            # utilities for rest apis
+        'rest_framework.authtoken',  # token authentication
+    )
+
+    # https://docs.djangoproject.com/en/2.0/topics/http/middleware/
+    MIDDLEWARE = (
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ALLOWED_HOSTS = ['*']
+    ROOT_URLCONF = 'gateway.urls'
+    SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+    WSGI_APPLICATION = 'gateway.wsgi.application'
+
+    # Email
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    ADMINS = (
+        ('Author', 'engineer@jpventura.com'),
+    )
+
+    # PostgreSQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': path.join(BASE_DIR, 'cornershop.sqlite3'),
+        }
+    }
+
+    # General
+    APPEND_SLASH = False
+    TIME_ZONE = 'UTC'
+    LANGUAGE_CODE = 'en-us'
+    # If you set this to False, Django will make some optimizations so as not
+    # to load the internationalization machinery.
+    USE_I18N = False
+    USE_L10N = True
+    USE_TZ = True
+    LOGIN_REDIRECT_URL = '/'
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/2.0/howto/static-files/
+    STATIC_ROOT = path.normpath(path.join(path.dirname(BASE_DIR), 'static'))
+    STATICFILES_DIRS = []
+    STATIC_URL = '/static/'
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+
+    # Media files
+    MEDIA_ROOT = path.join(path.dirname(BASE_DIR), 'media')
+    MEDIA_URL = '/media/'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': STATICFILES_DIRS,
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+    # Set DEBUG to False as a default for safety
+    # https://docs.djangoproject.com/en/dev/ref/settings/#debug
+    DEBUG = strtobool(os.getenv('DJANGO_DEBUG', 'no'))
+
+    # Password Validation
+    # https://docs.djangoproject.com/en/2.0/topics/auth/passwords/#module-django.contrib.auth.password_validation
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+    # Cornershop API User
+    AUTH_USER_MODEL = 'cornershop.apps.users.User'
+
+    # Logging
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'django.server': {
+                '()': 'django.utils.log.ServerFormatter',
+                'format': '[%(server_time)s] %(message)s',
+            },
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
+        },
+        'filters': {
+            'require_debug_true': {
+                '()': 'django.utils.log.RequireDebugTrue',
+            },
+        },
+        'handlers': {
+            'django.server': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'django.server',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler'
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'propagate': True,
+            },
+            'django.server': {
+                'handlers': ['django.server'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'django.request': {
+                'handlers': ['mail_admins', 'console'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'INFO'
+            },
+        }
+    }
+
+    # Django REST Framework
+    REST_FRAMEWORK = {
+        'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.TokenAuthentication',
+        ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ],
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer',
+        ),
+        'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
+    }